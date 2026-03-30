ab4054d5d58a5b7a5253dc06eb4196083103fe4c
Kloswasa <klos.wasa@gmail.com>
2026-03-30 22:07:32 +1100

    Update page.tsx

--- psychology-quiz/app/page.tsx
+++ psychology-quiz/app/page.tsx
@@ -2,7 +2,7 @@ import { HomePage } from '@/components/HomePage';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-neutral-200">
+    <div className="min-h-screen bg-neutral-200 max-h-[85dvh]">
       <HomePage />
       </div>
   );
