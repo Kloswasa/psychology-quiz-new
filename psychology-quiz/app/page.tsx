a6e2643a955ca9e4e2af8995114ccd9ab91fcc06
Kloswasa <klos.wasa@gmail.com>
2026-03-31 20:27:56 +1100

    Revert "testing responsiveness"
    This reverts commit 36920b0604d423b510bfa82dd9f20cc7ac4bf186.

--- psychology-quiz/app/page.tsx
+++ psychology-quiz/app/page.tsx
@@ -2,10 +2,8 @@ import { HomePage } from '@/components/HomePage';
 
 export default function Home() {
   return (
-    <div className="min-h-[100dvh] w-full bg-neutral-200 flex justify-center">
-      <div className="h-[100dvh] max-w-[430px] w-full overflow-hidden relative flex flex-col">
-        <HomePage />
+    <div className="min-h-screen bg-neutral-200 max-h-[85dvh]">
+      <HomePage />
       </div>
-    </div>
   );
 }
